ced5b8b7990ce999e631ad4d989c4ed06897dbbf
miku <miku@example.com>
2025-12-17 13:55:38 +0900

    add ./q16_templateLiteral.js

--- questions/q16_templateLiteral.js
+++ questions/q16_templateLiteral.js
@@ -1,9 +1,11 @@
-// Q16
-// テンプレートリテラルを使って、ユーザーの情報を含む挨拶文を作成してください
-const userName = "太郎";
-const age = 25;
+const users = [
+    { id: 1, name: "Alice", age: 20 },
+    { id: 2, name: "Bob", age: 25 },
+    { id: 3, name: "Charlie", age: 30 },
+    { id: 4, name: "Dave", age: 18 }
+];
 
-// ここにコードを書いてください
-// "こんにちは、太郎さん!あなたは25歳ですね。" という文字列を作成
-const greeting = `こんにちは、${userName}さん！あなたは${age}歳ですね`;
-console.log(greeting); // Expected: "こんにちは、太郎さん!あなたは25歳ですね。"
+// Q16. `users` 配列のデータから、各ユーザーの `name` だけを集めた新しい配列 `names` を作成してください。
+const names = users.map( (user) => user.name);
+
+console.log(names); // Expected: ["Alice", "Bob", "Charlie", "Dave"]
