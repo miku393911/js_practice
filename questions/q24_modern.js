b50bbd4d0183172fc24f5a4ab44f962bc6117785
miku <miku@example.com>
2025-12-18 19:41:34 +0900

    add ./q24_modern.js

---
+++ questions/q24_modern.js
@@ -0,0 +1,5 @@
+// Q24. 変数 `score` の値が 60以上であれば "Pass"、そうでなければ "Fail" という文字列を返す式を記述し、結果を `result` に代入してください。
+// (if文は使用しないこと)
+const score = 55;
+const result = score >= 60 ? "Pass" : "Fail";
+console.log(result); // Expected: Fail
