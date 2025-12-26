24dba1e5d3cb13ea42cac2977db809dc111d6410
miku <mik390625@gmail.com>
2025-12-26 20:58:27 +0900

    solve q05

---
+++ questions/vol4/q05_ifScore.js
@@ -0,0 +1,11 @@
+// 5. 変数 `score` (0-100) に応じて成績を出力してください。90以上: "S", 80以上: "A", 60以上: "B", 60未満: "C"。
+const score = 39;
+if(score >= 90){
+    console.log("S");
+}else if(score >= 80){
+    console.log("A");
+}else if(score >= 60){
+    console.log("B");
+}else{
+    console.log("C");
+}
