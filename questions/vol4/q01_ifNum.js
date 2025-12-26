8ad99bcc60bf27e5bad3f00cd3d39006a51c901d
miku <mik390625@gmail.com>
2025-12-26 18:20:54 +0900

    solve q01

---
+++ questions/vol4/q01_ifNum.js
@@ -0,0 +1,9 @@
+// 1. 変数 `num` が正の数なら「正」、負の数なら「負」、0なら「ゼロ」と出力してください。
+const num = 0;
+if(num > 0){
+    console.log("正");
+}else if(num < 0){
+    console.log("負");
+}else{
+    console.log("ゼロ");
+}
