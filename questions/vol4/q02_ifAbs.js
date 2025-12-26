843325321b0deb4b42cbdc9b08d93b4c925def9c
miku <mik390625@gmail.com>
2025-12-26 19:31:15 +0900

    solve q02

---
+++ questions/vol4/q02_ifAbs.js
@@ -0,0 +1,8 @@
+// 2. 変数 `num` の絶対値を計算して出力してください。（`Math.abs` は使用禁止。if文で実現すること）
+// Math.abs()はJavaScriptのメソッドで、絶対値を計算してくれる（absolute value: 絶対値
+const num = -3;
+if(num < 0){
+    console.log(num*(-1));
+}else{
+    console.log(num);
+}
