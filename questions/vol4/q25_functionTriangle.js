d7eaef1a82dce5cdff48102d6a28fb4c85ddda53
miku <mik390625@gmail.com>
2025-12-30 23:56:09 +0900

    solve q25

---
+++ questions/vol4/q25_functionTriangle.js
@@ -0,0 +1,3 @@
+// 25. 底辺 `base` と高さ `height` を受け取り、三角形の面積を返す関数 `getTriangleArea` を作成してください。
+const getTriangleArea =  (base, height) => (base * height) / 2;  // {}とreturnを付けても良い
+console.log(getTriangleArea(8, 4));
