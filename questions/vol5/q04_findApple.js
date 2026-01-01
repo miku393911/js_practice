c4db9460ff25605fd64a2240ca3c3a8f2386f140
miku <mik390625@gmail.com>
2026-01-01 20:35:50 +0900

    solve q04

--- questions/vol5/q04_findApple.js
+++ questions/vol5/q04_findApple.js
@@ -1,1 +1,18 @@
 // 4. 果物が入った配列 `const items = ["みかん", "バナナ", "りんご", "ぶどう"]` の中に「りんご」があるか探し、見つけたら「見つけました！」と表示して、その瞬間にループを終了するプログラムを書いてください。
+const items = ["みかん", "バナナ", "りんご", "ぶどう"];
+for(let i = 0; i< items.length; i++){
+    if(items[i] === "りんご"){
+        console.log("見つけました！");
+        break;
+    }
+}
+
+// .includes()メソッドを使うと...
+if(items.includes("りんご")){
+    console.log("見つけました！");
+}
+// .find()メソッド使うと...
+const foundItem = items.find((item) => item === "りんご");
+if(foundItem){
+    console.log("見つけました！");
+}
