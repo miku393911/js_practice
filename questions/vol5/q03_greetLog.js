21e533e67e58db314340a5a217ce12a74d5d467d
miku <mik390625@gmail.com>
2026-01-01 19:37:52 +0900

    solve q03

--- questions/vol5/q03_greetLog.js
+++ questions/vol5/q03_greetLog.js
@@ -1,1 +1,12 @@
 // 3. 名前が入った配列 `const names = ["田中", "佐藤", "鈴木"]` を使い、全員分「こんにちは、〇〇さん」と出力するプログラムを書いてください。
+const names = ["田中", "佐藤", "鈴木"];
+for(let i = 0; i < names.length; i++){
+    console.log(`こんにちは、${names[i]}さん`);
+}
+
+// for ofを使うと...
+for(const name of names){
+    console.log(`こんにちは、${name}さん`);
+}
+// .forEachメソッド()を使うと...
+names.forEach((name) => console.log(`こんにちは、${name}さん`));
